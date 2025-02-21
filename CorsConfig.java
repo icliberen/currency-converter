658c7eec10db1cafcf6db65757aeb538ae2a1de1
icliberen <icliberen@gmail.com>
2025-02-22 01:36:23 +0300

    Fixed CORS issues for frontend API requests

--- CorsConfig.java
+++ CorsConfig.java
@@ -4,7 +4,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.Collections;
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
@@ -13,9 +13,9 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("*"));
-        config.setAllowedHeaders(Collections.singletonList("*"));
-        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://currency-converter-production-239c.up.railway.app"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         source.registerCorsConfiguration("/api/**", config);
         return new CorsFilter(source);
     }
